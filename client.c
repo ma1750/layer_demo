ca914d68e22e8fe47aef3123bd299fed26a0da4e
ma1750 <mc.ma1750@gmail.com>
2021-05-04 17:37:38 +0900

    client send TCP/UDP packet

--- client.c
+++ client.c
@@ -8,6 +8,10 @@
 #include "global.h"
 #include "md5.h"
 
+#define VERSION 9999
+#define TTL 100
+#define APP_TYPE_CODE 1234
+
 int get_file_length(FILE*);
 int enclose_ip(char*, char*, int, int, int, int);
 int enclose_tcp(char*, char*, int, int);
@@ -16,8 +20,8 @@ void gen_MD5(unsigned char*, char *, int);
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 2) {
-        fprintf(stderr, "[Usage] %s file_name", argv[0]);
+    if (argc != 3) {
+        fprintf(stderr, "[Usage] %s file_name type\n\ttype| TCP: 0, UDP: 1\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
@@ -32,14 +36,64 @@ int main(int argc, char const *argv[])
     }
 
     int length = get_file_length(fp);
-    char file_buf[length];
+    char file_buf[length+1];
     size_t ret = fread(file_buf, 1, length, fp);
+    file_buf[length+1] = '\0';
     if (ret != length) {
         perror("fread() failed");
         fclose(fp);
         exit(EXIT_FAILURE);
     }
     fclose(fp);
+
+    int type = atoi(argv[2]);
+    int buf_length = 0;
+    char *sendbuf;
+    if (type == 0) {
+        // TCP
+        sendbuf = (char*)malloc(sizeof(char) * (52+length+1));
+        if (sendbuf == NULL) {
+            perror("malloc() failed");
+            free(sendbuf);
+            exit(EXIT_FAILURE);
+        }
+        char tcp_header[40];
+        buf_length = enclose_tcp(tcp_header, file_buf, length, APP_TYPE_CODE);
+        if (buf_length != 40+(length+1)) {
+            fprintf(stderr, "enclose_tcp() fialed\n");
+            free(sendbuf);
+            exit(EXIT_FAILURE);
+        }
+
+        length = enclose_ip(sendbuf, tcp_header, buf_length, type, VERSION, TTL);
+        if (length != 12+buf_length) {
+            fprintf(stderr, "enclose_ip() fialed\n");
+            free(sendbuf);
+            exit(EXIT_FAILURE);
+        }
+    } else if (type == 1) {
+        // UDP
+        sendbuf = (char*)malloc(sizeof(char) * (20+length+1));
+        if (sendbuf == NULL) {
+            perror("malloc() failed");
+            free(sendbuf);
+            exit(EXIT_FAILURE);
+        }
+        char udp_header[8];
+        buf_length = enclose_udp(udp_header, file_buf, length, APP_TYPE_CODE);
+        if (buf_length != 8+(length+1)) {
+            fprintf(stderr, "enclose_udp() fialed\n");
+            free(sendbuf);
+            exit(EXIT_FAILURE);
+        }
+
+        length = enclose_ip(sendbuf, udp_header, buf_length, type, VERSION, TTL);
+        if (length != 12+buf_length) {
+            fprintf(stderr, "enclose_ip() fialed\n");
+            free(sendbuf);
+            exit(EXIT_FAILURE);
+        }
+    }
 
     sockfd = socket(AF_UNIX,SOCK_STREAM,0);
     if (sockfd == -1) {
@@ -58,8 +112,9 @@ int main(int argc, char const *argv[])
         exit(1);
     }
 
-    write(sockfd, file_buf, sizeof(file_buf));
+    write(sockfd, sendbuf, length);
     close(sockfd);
+    free(sendbuf);
     exit(0);
 }
 
@@ -90,36 +145,36 @@ int enclose_ip(char *ret_ptr, char *payload_ptr, int payload_len, int type, int 
     int ret_len = 0;
     ret_len = snprintf(ret_ptr, 12+payload_len,
                         "%04d%04d%04d%s", type, version, ttl, payload_ptr);
-    return ret_len;
+    return ret_len+1;
 }
 
 
 int enclose_tcp(char *ret_ptr, char *payload_ptr, int payload_len, int type)
 {
     int ret_len = 0;
-    char ret[40+payload_len];
+    char ret[40+payload_len+1];
     unsigned char digest[16];
     gen_MD5(digest, payload_ptr, payload_len);
 
-    ret_len = snprintf(ret, 8, "%04d%04d", type, payload_len);
+    ret_len = snprintf(ret, 9, "%04d%04d", type, payload_len);
     char tmp[2];
-    for (int i = 0; i < 16; ++i) {
-        snprintf(tmp, 4, "%02x", digest[i]);
-        ret[8+2*(i-1)] = tmp[0];
-        ret[8+2*i-1] = tmp[1];
+    for (int i = 1; i <= 16; ++i) {
+        snprintf(tmp, 4, "%02x", digest[i-1]);
+        ret[7+2*i-1] = tmp[0];
+        ret[7+2*i] = tmp[1];
     }
 
-    ret_len = snprintf(ret_ptr, 40+payload_len, "%s%s", ret, payload_ptr);
-    return ret_len;
+    ret_len = snprintf(ret_ptr, 40+payload_len+1, "%s%s", ret, payload_ptr);
+    return ret_len+1;
 }
 
 
 int enclose_udp(char *ret_ptr, char *payload_ptr, int payload_len, int type)
 {
     int ret_len = 0;
-    ret_len = snprintf(ret_ptr, 8+payload_len, "%04d%04d%s",
+    ret_len = snprintf(ret_ptr, 8+payload_len+1, "%04d%04d%s",
                         type, payload_len, payload_ptr);
-    return ret_len;
+    return ret_len+1;
 }
 
 
