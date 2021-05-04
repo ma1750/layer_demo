9bdd116769bd9b2367ebad2afe6f0165d069a31a
ma1750 <mc.ma1750@gmail.com>
2021-05-04 18:23:09 +0900

    unpack_ip()

--- server.c
+++ server.c
@@ -1,9 +1,34 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/un.h>
 #include <unistd.h>
 #include <string.h>
+
+
+typedef struct
+{
+    int type;
+    int version;
+    int ttl;
+} ip_t;
+
+typedef struct
+{
+    int type;
+    int len;
+    char digest[32];
+} tcp_t;
+
+typedef struct
+{
+    int type;
+    int len;
+} udp_t;
+
+
+void unpack_ip(ip_t*, char*);
 
 int main(void)
 {
@@ -30,6 +55,27 @@ int main(void)
 
         read(client_sockfd,recv_buf,1024);
         printf("recv: %s\n", recv_buf);
+        ip_t ip;
+        unpack_ip(&ip, recv_buf);
         close(client_sockfd);
     }
+}
+
+
+void unpack_ip(ip_t *ret_ptr, char *msg_ptr)
+{
+    char type[5];
+    char version[5];
+    char ttl[5];
+
+    strncpy(type, msg_ptr, 4);
+    type[4] = '\0';
+    strncpy(version, msg_ptr+4, 4);
+    version[4] = '\0';
+    strncpy(ttl, msg_ptr+8, 4);
+    ttl[4] = '\0';
+
+    ret_ptr -> type = atoi(type);
+    ret_ptr -> version = atoi(version);
+    ret_ptr -> ttl = atoi(ttl);
 }
